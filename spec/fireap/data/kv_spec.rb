f000be8058ca3907e81ce73c8dea3ce8c16269e8
key-amb <yasutake.kiyoshi@gmail.com>
2016-03-17 14:38:46 +0900

    Add case when Value is not set

--- spec/fireap/data/kv_spec.rb
+++ spec/fireap/data/kv_spec.rb
@@ -2,19 +2,35 @@ require 'fireap/data/kv'
 
 require 'base64'
 
-kv = {
-  'Key'   => 'version',
-  'Value' => Base64.encode64('v0.1.0'),
-}
+describe 'Transform to Model::Kv' do
 
-raw  = Fireap::Data::Kv.new(kv)
-data = raw.to_model
-
-describe 'New Kv raw/data' do
-  it 'data is a Fireap::Model::Kv' do
-    expect(data).to be_an_instance_of(Fireap::Model::Kv)
+  describe 'Valid Kv kv_m' do
+    kv = {
+      'Key'   => 'version',
+      'Value' => Base64.encode64('v0.1.0'),
+    }
+    data = Fireap::Data::Kv.new(kv)
+    kv_m = data.to_model
+    it 'kv_m is a Fireap::Model::Kv' do
+      expect(kv_m).to be_an_instance_of(Fireap::Model::Kv)
+    end
+    it 'kv_m.value is decoded' do
+      expect(kv_m.value).to eq 'v0.1.0'
+    end
   end
-  it 'data.value is decoded' do
-    expect(data.value).to eq 'v0.1.0'
+
+  describe 'Value is undefined' do
+    kv = {
+      'Key'   => 'version',
+      'Value' => nil,
+    }
+    data = Fireap::Data::Kv.new(kv)
+    kv_m = data.to_model
+    it 'kv_m is a Fireap::Model::Kv' do
+      expect(kv_m).to be_an_instance_of(Fireap::Model::Kv)
+    end
+    it 'kv_m.value is nil' do
+      expect(kv_m.value).to be nil
+    end
   end
 end
